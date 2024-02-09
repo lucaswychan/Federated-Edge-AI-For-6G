c9e06c73690dc4d6c73a09592f582bc9f4766cd8
LConann <chanconan@gmail.com>
2024-02-09 15:48:34 +0800

    add factory for design pattern

---
+++ algorithm/algorithm_factory.py
@@ -0,0 +1,115 @@
+import numpy as np
+
+from algorithm.algorithm_base import Algorithm
+from algorithm.fedavg import FedAvg
+from algorithm.feddyn import FedDyn
+from algorithm.fedprox import FedProx
+from algorithm.scaffold import SCAFFOLD
+
+
+class AlgorithmFactory:
+    def __init__(self, args):
+        self.args = args
+
+    def create_algorithm(self, algorithm_name) -> Algorithm:
+        algorithm = None
+        # FedDyn parameter
+        if algorithm_name == "FedDyn":
+            alpha_coef = 1e-2
+
+            algorithm = FedDyn(
+                self.args.act_prob,
+                self.args.lr,
+                self.args.lr_decay_per_round,
+                self.args.batch_size,
+                self.args.epoch,
+                self.args.weight_decay,
+                self.args.model_func,
+                self.args.init_model,
+                self.args.data_obj,
+                self.args.n_param,
+                self.args.max_norm,
+                self.args.air_comp,
+                self.args.noiseless,
+                self.args.save_period,
+                self.args.print_per,
+                alpha_coef,
+            )
+
+        # FedProx parameter
+        elif algorithm_name == "FedProx":
+            mu = 1e-4
+
+            algorithm = FedProx(
+                self.args.act_prob,
+                self.args.lr,
+                self.args.lr_decay_per_round,
+                self.args.batch_size,
+                self.args.epoch,
+                self.args.weight_decay,
+                self.args.model_func,
+                self.args.init_model,
+                self.args.data_obj,
+                self.args.n_param,
+                self.args.max_norm,
+                self.args.air_comp,
+                self.args.noiseless,
+                self.args.save_period,
+                self.args.print_per,
+                mu,
+            )
+
+        # SCAFFOLD parameter
+        elif algorithm_name == "SCAFFOLD":
+            n_data_per_client = (
+                np.concatenate(self.args.data_obj.clnt_x, axis=0).shape[0]
+                / self.args.n_clients
+            )
+            n_iter_per_epoch = np.ceil(n_data_per_client / self.args.batch_size)
+            n_minibatch = (self.args.epoch * n_iter_per_epoch).astype(np.int64)
+            self.args.print_per = self.args.print_per * n_iter_per_epoch
+            global_learning_rate = 1
+
+            algorithm = SCAFFOLD(
+                self.args.act_prob,
+                self.args.lr,
+                self.args.lr_decay_per_round,
+                self.args.batch_size,
+                self.args.epoch,
+                self.args.weight_decay,
+                self.args.model_func,
+                self.args.init_model,
+                self.args.data_obj,
+                self.args.n_param,
+                self.args.max_norm,
+                self.args.air_comp,
+                self.args.noiseless,
+                self.args.save_period,
+                self.args.print_per,
+                n_minibatch,
+                global_learning_rate,
+            )
+
+        elif algorithm_name == "FedAvg":
+            algorithm = FedAvg(
+                self.args.act_prob,
+                self.args.lr,
+                self.args.lr_decay_per_round,
+                self.args.batch_size,
+                self.args.epoch,
+                self.args.weight_decay,
+                self.args.model_func,
+                self.args.init_model,
+                self.args.data_obj,
+                self.args.n_param,
+                self.args.max_norm,
+                self.args.air_comp,
+                self.args.noiseless,
+                self.args.save_period,
+                self.args.print_per,
+            )
+
+        else:
+            raise ValueError(f"Unknown algorithm name: {self.algorithm_name}")
+
+        return algorithm
